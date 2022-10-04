101e9f767d03a19de348510ffe7c4d2774a8c710
veselinovicComtrade <112486980+veselinovicComtrade@users.noreply.github.com>
2022-10-04 11:53:18 +0200

    Update calculate.groovy

--- vars/calculate.groovy
+++ vars/calculate.groovy
@@ -1,7 +1,7 @@
 def add(n1,n2) {
-  echo "Sum of these two numbers is ${n1+n2}
+  echo "Sum of these two numbers is ${n1+n2}"
 }
 
 def mul(n1,n2) {
-  echo "Multiplication of these two numbers is ${n1*n2}
+  echo "Multiplication of these two numbers is ${n1*n2}"
 }
